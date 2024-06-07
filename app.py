aab68ecb419cdde70fd0b03d4f9aaa415c3d515a
Jana Wolfová <janarega@seznam.cz>
2024-06-07 10:11:08 +0200

    Initial commint

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, request, render_template  
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+
+"""
+Načítání dat z formuláře, který uživatel vyplní a odešle. 
+vytvoříme jednoduchý formulář v HTML a připravíme Flask aplikaci, 
+aby tato data přijímala.
+"""
+
+app = Flask(__name__)
+
+@app.route("/") # určí URL cestu, na kterou se má daná funkce navázat.
+
+def index(): #func zobrazí hlavní stránku, která obsahuje formulář
+    return render_template("index.html")   #vykreslení HTML šablon
+
+
+@app.route("/search", methods=["POST"]) #přijme data z formuláře
+
+def search():
+    query = request.form["query"]  # načte klíčové slovo
+    html = google_search(query)  # provede vyhledávání na Googlu
+    results = process_results(html)  # zpracuje výsledky vyhledávání
+    
+    return render_template("results.html", results=results)  # zobrazí výsledky na nové stránce
+
+
+
+def google_search(query):
+    """
+    funkce na vyhledávání google 
+    """
+    formatted_query = query.replace(" ", "+")
+    url = f"https://www.google.com/search?q={formatted_query}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    response = requests.get(url, headers=headers)
+
+
+
+    return response.text
+
+
+def process_results(html) -> dict:
+    """
+    funkce z beautifulsoup rozděluje text na html stránce podle  a uloží do seznamu slovníků.
+    """
+    soup = BeautifulSoup(html, "html.parser")
+    results = []
+
+    for g in soup.find_all(class_="g"):
+        title = g.find("h3")
+        link = g.find("a")["href"]
+        snippet = g.find(class_="aCOpRe")
+
+        if title and link:
+            results.append({
+                "title": title.text,
+                "link": link,
+                "snippet": snippet.text if snippet else ''
+            })
+
+    return results
+
+def format_and_save_to_json(results):
+    # Převést seznam slovníků na řetězec JSON
+    formatted_data = json.dumps(results, indent=4, ensure_ascii=False)
+
+    # Uložit řetězec JSON do souboru
+    with open("results.json", mode="w", encoding="utf-8") as file:
+        file.write(formatted_data)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
